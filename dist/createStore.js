af40457d75884a7b21073951a763e9d3d9673d94
Ge Yang <yangge1987@gmail.com>
2015-12-18 21:32:31 -0600

    update dist

--- dist/createStore.js
+++ dist/createStore.js
@@ -1,5 +1,7 @@
 var Store_1 = require("./Store");
 function createStore(reducer, initialState) {
-    return new Store_1.Store(reducer, initialState);
+    return function () {
+        return new Store_1.Store(reducer, initialState);
+    };
 }
 exports.createStore = createStore;
