22a761c14470d55821e09eb435805f955100b29e
Ge Yang <yangge1987@gmail.com>
2015-12-19 21:12:25 -0600

    change StoreService to class

--- src/StoreService.ts
+++ src/StoreService.ts
@@ -10,20 +10,22 @@
 import {Reducer} from "./interfaces";
 import {Hash} from "./interfaces";
 
-export interface StoreService<TState> {
+export class StoreService<TState> {
     initialState:TState;
     reducer:Reducer;
     types:Hash<string>;
     $:Hash<any>;
     actionsCreators: Hash<any>;
 
-    // # Typical coding patterns in the constructor:
-    //
-    // 1. Compose the reducer of your dependencies and save it to this.reducer
-    // 2. Collect all streams from lower level dependencies to this.$
-    // 3. Now initialize your store if this is the root store, or assemble the child states
-    //    and assign it to `this.initialState`.
-    // 4. It is also convenient to collect actionCreators. They will be dispatched with
-    //    `this` keyword bound to the rootStore object.
-    //
+    constructor () {
+        // # Typical coding patterns in the constructor:
+        //
+        // 1. Compose the reducer of your dependencies and save it to this.reducer
+        // 2. Collect all streams from lower level dependencies to this.$
+        // 3. Now initialize your store if this is the root store, or assemble the child states
+        //    and assign it to `this.initialState`.
+        // 4. It is also convenient to collect actionCreators. They will be dispatched with
+        //    `this` keyword bound to the rootStore object.
+        //
+    }
 }
