f9bb9cd911b5e4979035346d0e8751591bb87aa6
Ge Yang <yangge1987@gmail.com>
2016-03-26 17:56:42 -0500

    FORMATTING reformat test

--- src/Store.subscription.spec.ts
+++ src/Store.subscription.spec.ts
@@ -4,7 +4,7 @@
 export default {}
 
 import {Action, Hash, Reducer, Store} from "./index";
-let reducer = <Reducer>function (state:number, action:Action, callback:(state:number)=>void):number {
+let reducer = <Reducer>function (state:number = 0, action:Action, callback:(state:number)=>void):number {
     if (action.type === "INC") {
         return state + 1;
     } else if (action.type === "DEC") {
@@ -18,23 +18,36 @@ import {Observable} from 'rxjs';
 
 describe("store$", function () {
     it("can get updated state", function () {
-        let store$:Store<number> = new Store(reducer, 10);
+
+        let store$:Store<number> = new Store(reducer);
         store$.subscribe(state => console.log("test 1: ", state));
+
     });
     it("can get updated state as well as actions", function () {
+
         let store$:Store<number> = new Store(reducer, 10);
-        store$.map(store => {
-            return {store, action: store$.action$.getValue()};
-        }).subscribe(state => console.log("test 2: ", state));
+        store$
+            .map(store => ({store, action: store$.action$.getValue()}))
+            .subscribe(state => console.log("test 2: ", state));
+
     });
     it("can subscribe to actions", function () {
         let store$:Store<number> = new Store(reducer, 10);
 
-        let testAction$ = Observable.from([{type: "INC"}, {type: "DEC"}, {type: "INC"}, {type: "DEC"}]);
-        store$.map(store => {
-            return {store, action: store$.action$.getValue()};
-        }).subscribe(state => console.log("test 3: ", state));
-        testAction$.subscribe(action => store$.dispatch(action));
+        let testAction$ = Observable.from([
+            {type: "INC"},
+            {type: "DEC"},
+            {type: "INC"},
+            {type: "DEC"}
+        ]);
+        store$
+            .map(store => {
+                return {store, action: store$.action$.getValue()};
+            })
+            .subscribe(state => console.log("test 3: ", state));
+
+        testAction$
+            .subscribe(action => store$.dispatch(action));
     });
 });
 
