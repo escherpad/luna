3dd0e6ec38456e22e5ef25a18c5e2a9c1443a188
Ge Yang <yangge1987@gmail.com>
2017-12-21 02:51:37 -0600

    Update build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
 /** Created by ge on 12/4/15. */
 export * from "./interfaces";
-export * from "./util/combineReducers";
-export * from "./Store";
-export * from "./createStore";
+export { combineReducers, passOrCombineReducers } from "./util/combineReducers";
+export { Store, INIT_STORE, INIT_STORE_ACTION } from "./Store";
+export { createStore } from "./createStore";
--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,14 @@
 "use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
 /** Created by ge on 12/4/15. */
-__export(require("./util/combineReducers"));
-__export(require("./Store"));
-__export(require("./createStore"));
+var combineReducers_1 = require("./util/combineReducers");
+exports.combineReducers = combineReducers_1.combineReducers;
+exports.passOrCombineReducers = combineReducers_1.passOrCombineReducers;
+var Store_1 = require("./Store");
+exports.Store = Store_1.Store;
+exports.INIT_STORE = Store_1.INIT_STORE;
+exports.INIT_STORE_ACTION = Store_1.INIT_STORE_ACTION;
+var createStore_1 = require("./createStore");
+exports.createStore = createStore_1.createStore;
 /*remove dependency on angular2*/
 //export {provideStore} from "./ng2/provideStore";
 //export {StoreService} from "./ng2/StoreService";
